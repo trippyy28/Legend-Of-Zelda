d0d3efd37919df8f8b2dfd74b36b86ea0ca29c6c
user <user@users-MacBook-Pro.local>
2023-06-20 21:54:59 +0300

    adding start menu

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ import { drawMap, map7_7 } from "./Map.js";
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let gameState = "menu";
+let spriteWidth, spriteHeight, spriteX, spriteY;
+let startM = new Image();
+let numSprites = 2240 / 224;
+let spriteCounter = 0;
+let frameCounter = 0;
+startM.src = "/assets/menu-sprite.png";
+startM.onload = function () {
+  // dimensions of each sprite
+  spriteWidth = 224;
+  spriteHeight = 224;
+
+  // coordinates of the sprite you want to draw within the sprite sheet
+  spriteX = 0; // for the first sprite, this would be 0
+  spriteY = 0; // for the first sprite, this would be 0
+  startGame();
+};
 let keys = {
   ArrowLeft: false,
   ArrowUp: false,
@@ -15,6 +31,9 @@ document.addEventListener(
   "keydown",
   function (e) {
     if (keys.hasOwnProperty(e.key)) keys[e.key] = true;
+    if (e.key === "Enter") {
+      gameState = "play";
+    }
   },
   false
 );
@@ -29,8 +48,6 @@ let worldTiles = new Image();
 worldTiles.src = "/assets/tiles-overworld.png";
 let link1 = new Image();
 link1.src = "/assets/sprite.png";
-let startM = new Image();
-startM.src = "/assets/menu.png";
 let linkY = 135;
 let linkX = 116;
 
@@ -41,7 +58,26 @@ function draw() {
   ctx.fillStyle = "rgb(20,20,20)";
   ctx.fillRect(0, 0, 256, 240);
   if (gameState === "menu") {
-    ctx.drawImage(startM, 0, -20, 1000, 1000);
+    let spriteX = (spriteCounter % numSprites) * spriteWidth;
+    let canvasX = 0;
+    let canvasY = 0;
+
+    ctx.drawImage(
+      startM,
+      spriteX,
+      spriteY,
+      spriteWidth,
+      spriteHeight,
+      canvasX,
+      canvasY,
+      spriteWidth,
+      spriteHeight
+    );
+    if (frameCounter % 10 === 0) {
+      // only increment spriteCounter every 10 frames
+      spriteCounter++;
+    }
+    frameCounter++;
   } else if (gameState === "play") {
     if (keys.ArrowLeft) link.move("left", map7_7);
     if (keys.ArrowRight) link.move("right", map7_7);
